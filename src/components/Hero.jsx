eea491415670d2bd2f209b591e9c66a407de8c47
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-02-16 18:08:14 -0500

    Replaced IMG

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import logo from '../assets/images/logo.png';
 import img1 from '../assets/images/img1.jpeg';
-import img2 from '../assets/images/img2.jpeg';
+import img2 from '../assets/images/img2.jpg';
 import img3 from '../assets/images/img3.jpeg';
 import img4 from '../assets/images/img4.png';
 import img5 from '../assets/images/img5.jpg';
